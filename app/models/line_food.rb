40587dc7cba97184ce28f3ebcdd142dd985e5876
Atsu <kamnknon@gmail.com>
2021-06-09 18:58:47 +0900

    line_foodコントローラー作成

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -1,12 +1,12 @@
-class LineFoods < ApplicationRecord
+class LineFood < ApplicationRecord
   belongs_to :food
   belongs_to :restaurant
   belongs_to :order, optional: true
 
-  validate :count, numericality: {greater_than: 0}
+  validates :count, numericality: {greater_than: 0}
 
   scope :active, -> {where(active: true)}
-  scope :other_restaurants, -> (picked_restaurant_id){where.not(restaurant_id: picked_restaurant_id)}
+  scope :other_restaurant, -> (picked_restaurant_id){where.not(restaurant_id: picked_restaurant_id)}
 
   def total_amount
     food.price * count
