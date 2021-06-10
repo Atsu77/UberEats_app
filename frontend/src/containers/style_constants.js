5865f13cbc6b1d6c23fafe6644f00edcc72aae47
Atsu <kamnknon@gmail.com>
2021-06-10 10:25:33 +0900

    apiアクセス用の関数作成

---
+++ frontend/src/containers/style_constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  MAIN: "#02A457",
+  BORDER: "#e2e2e2",
+  SUB_TEXT: "#545454",
+  SUB_BUTTUN: "#eee",
+};
+
+export const FONT_SIZE = {
+  STAND_BODY: "24px",
+  BODY1: "16px",
+  BODY: "14px",
+};
