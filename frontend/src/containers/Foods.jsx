855282eb2c142a78acf216d0457230d0cd90bd1f
Atsu <kamnknon@gmail.com>
2021-07-15 11:00:45 +0900

    Food一覧のAPIを叩く処理を追加

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,47 @@
-import React  from 'react';
+import React, { useEffect, useReducer } from "react";
 
-export const Foods = ({
-  match
-}) => {
+// reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTypes,
+  foodsReducer,
+} from "../reducers/foods";
+
+//apis
+import { fetchFoods } from "../apis/foods";
+
+//constants
+import { REQUEST_STATE } from "../constants";
+
+export const Foods = ({ match }) => {
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  useEffect(() => {
+    dispatch({ type: foodsActionTypes.FETCHING });
+    fetchFoods(match.params.restaurantsId).then((data) => {
+      dispatch({
+        type: foodsActionTypes.FETCHING_SUCCESS,
+        payload: {
+          foods: data.foods,
+        },
+      });
+    });
+  }, []);
+
   return (
     <>
-      フード一覧
-      <p>
-      restaurantsIdは {match.params.restaurantsId} です
-      </p>
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+        <>
+        <p>ロード中</p>
+        </>
+        :
+        foodsState.foodsList.map(food => 
+          <div key={food.id}>
+            {food.name}
+          </div>
+          )
+      }
     </>
-  )
-}
+  );
+};
