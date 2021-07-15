d8fd47652c43a25cb7a5ac0f892865a853d4cce8
Atsu <kamnknon@gmail.com>
2021-07-15 10:25:09 +0900

    レストラン一覧ページの作成

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React  from 'react';
 
 export const Foods = ({
   match
 }) => {
   return (
-    <Fragment>
+    <>
       フード一覧
       <p>
       restaurantsIdは {match.params.restaurantsId} です
       </p>
-    </Fragment>
+    </>
   )
 }
