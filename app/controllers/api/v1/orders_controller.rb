79b0612d5160a5af5f3c421d8befeb4df18babd8
Atsu <kamnknon@gmail.com>
2021-07-18 13:24:01 +0900

    注文機能を追加

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,9 +2,10 @@ module Api
   module V1
     class OrdersController < ApplicationController
       def create
-        posted_line_foods = LineFoods.where(id: params[:line_food_ids])
+        posted_line_foods = LineFood.where(id: params[:line_food_ids])
         order = Order.new(
-          total_price: total_price(posted_line_foods),
+          restaurant_id: posted_line_foods.first.restaurant_id,
+          total_price: total_price(posted_line_foods)
         )
         if order.save_with_update_line_foods!(posted_line_foods)
           render json: {}, status: :no_content
@@ -14,8 +15,9 @@ module Api
       end
 
       private
+
       def total_price(posted_line_foods)
-        posted_line_foods.sum{|line_food| line_food.total_amount} + posted_line_foods.first.restaurant.fee
+        posted_line_foods.sum { |line_food| line_food.total_amount } + posted_line_foods.first.restaurant.fee
       end
     end
   end
