f655bd87d186d7dcf569f61c791b222848cdffee
Atsu <kamnknon@gmail.com>
2021-07-18 13:54:25 +0900

    orderモデルを微調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_121222) do
+ActiveRecord::Schema.define(version: 2021_07_18_042458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_06_08_121222) do
     t.integer "total_price", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2021_06_08_121222) do
   add_foreign_key "foods", "restaurants"
   add_foreign_key "line_foods", "foods"
   add_foreign_key "line_foods", "restaurants"
+  add_foreign_key "orders", "restaurants"
 end
